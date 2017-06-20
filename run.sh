cb44f0c65319f11170c2456c32cff22d6de0cb2a
oven-yang <slyangwen@163.com>
2017-06-20 08:51:49 +0800

    modify run shell

--- run.sh
+++ run.sh
@@ -4,15 +4,15 @@ make log-analysis
 make node-define
 rm -f ./source/log/*
 
-minoperation=300
-maxoperation=500
+minoperation=200
+maxoperation=400
 opplus=50
 minkind=26
 maxkind=26
-# topologyarr=("CERNET2" "Deltacom" "GtsCe" "Oteglobe")
-# nodes=(20 113 149 93)
-topologyarr=("Deltacom")
-nodes=(113)
+topologyarr=("CERNET2" "Deltacom" "GtsCe" "Oteglobe")
+nodes=(20 113 149 93)
+# topologyarr=("Deltacom")
+# nodes=(113)
 strategyarr=("PPDS" "LRU" "no-cache" "LRU-random")
 # strategyarr=("no-cache")
 
@@ -24,7 +24,6 @@ do
 	do
         rm -f ./doc/data/${topologyarr[$i]}.topology/${strategyarr[$j]}/*
         rm -r ./source/backup-log/${topologyarr[$i]}.topology/${strategyarr[$j]}/*
-        rm source/log/*
         req=$minoperation
         while [ $req -le $maxoperation ]
         do
@@ -35,6 +34,7 @@ do
 	done
 	i=`expr $i + 1`
 done
+rm source/log/*
 echo "old data removed"
 
 i=0
@@ -49,7 +49,7 @@ do
         while [ $j -lt ${#strategyarr[*]} ]
         do
             ./cache ${topologyarr[${i}]} ${strategyarr[${j}]}
-            ./log-analysis $req ${topologyarr[${i}]} ${strategyarr[${j}]}
+            ./log-analysis $minkind $req ${topologyarr[${i}]} ${strategyarr[${j}]}
             mv ./source/log/* ./source/backup-log/${topologyarr[$i]}.topology/${strategyarr[$j]}/$req
             echo "$req ${topologyarr[${i}]} ${strategyarr[${j}]} finished"
             j=`expr $j + 1`
